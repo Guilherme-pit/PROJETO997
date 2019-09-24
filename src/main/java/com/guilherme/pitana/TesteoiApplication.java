042547e06b658954ee0cd3b38e1f5205edca5fef
Guilherme-pit <guilhermepitana@outlook.com>
2019-09-24 13:47:14 -0300

    Projeto foi criado

---
+++ src/main/java/com/guilherme/pitana/TesteoiApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.pitana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteoiApplication.class, args);
+	}
+
+}
